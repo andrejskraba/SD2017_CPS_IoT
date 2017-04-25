de2ca2104822466c23c59d40b99114ce1e30e01c
root <andrej.skraba@gmail.com>
2017-04-25 11:10:05 +0000

    Primeri do DC motor z gumbi

---
+++ primer10.js
@@ -0,0 +1,17 @@
+var http = require("http").createServer(handler); // ob zahtevi req -> handler
+var firmata = require("firmata");
+var fs = require("fs"); // knjižnjica za delo z datotekami (File System fs)
+
+function handler(req, res) {
+    fs.readFile(__dirname + "/primer10.html",
+    function(err, data) {
+        if (err) {
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            return res.end("Napaka pri nalaganju html strani!");
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+http.listen(8080); // strežnik bo poslušal na vratih 8080
