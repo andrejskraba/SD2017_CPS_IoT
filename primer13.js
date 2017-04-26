eef54a0f846accab24c18f187bd455cc95d97e24
root <andrej.skraba@gmail.com>
2017-04-26 12:08:32 +0000

    Primeri od dveh potenciometrov dalje

---
+++ primer13.js
@@ -0,0 +1,55 @@
+var http = require("http").createServer(handler); // ob zahtevi req -> handler
+var firmata = require("firmata");
+var fs = require("fs"); // knjižnjica za delo z datotekami (File System fs)
+var io = require("socket.io").listen(http); // knjiž. za komunik. prek socket-a 
+
+console.log("Priklop Arduina");
+
+var board = new firmata.Board("/dev/ttyACM0", function(){
+    console.log("Aktiviramo analogni pin 0");
+    board.pinMode(0, board.MODES.ANALOG);
+    console.log("Aktiviramo analogni pin 1");
+    board.pinMode(1, board.MODES.ANALOG);
+});
+
+function handler(req, res) {
+    fs.readFile(__dirname + "/primer13.html",
+    function(err, data) {
+        if (err) {
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            return res.end("Napaka pri nalaganju html strani!");
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+http.listen(8080); // strežnik bo poslušal na vratih 8080
+
+var želenaVrednost = 0; // želeno vrednost postavimo na 0
+var dejanskaVrednost = 0; // dejansko vrednost postavimo na 0
+
+console.log("Zagon sistema"); // izpis sporočila o zagonu
+
+board.on("ready", function(){
+    console.log("Plošča pripravljena");
+    board.analogRead(0, function(value){
+        želenaVrednost = value; // neprekinjeno branje pina A0
+    });
+    board.analogRead(1, function(value){
+        dejanskaVrednost = value; // neprekinjeno branje pina A1
+    });
+    
+    io.sockets.on("connection", function(socket){
+        setInterval(pošljiVrednosti, 40, socket); // na 40ms pošlj. vred.
+    });
+    
+});
+
+function pošljiVrednosti(socket) {
+    socket.emit("klientBeriVrednosti",
+    {
+        "želenaVrednost": želenaVrednost,
+        "dejanskaVrednost": dejanskaVrednost
+    });
+};
